9f6f6ce555840bf66f7dc90dba3a6319dd84e5e5
Antoine Souesme <antoine.souesme@gmail.com>
2026-04-14 13:22:21 +0200

    feature: Ajout d'analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,56 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import { buildPageMetadata, seoKeywords, siteUrl } from "./lib/seo";
 
 const defaultMetadata = buildPageMetadata({
-  title:
-    "Développeur freelance en Centre Bretagne | Site internet et logiciel sur mesure",
-  description:
-    "Développeur freelance en Centre Bretagne. Création de site internet et développement logiciel sur mesure pour entreprises en Bretagne.",
-  path: "/",
-  keywords: seoKeywords,
+    title:
+        "Développeur freelance en Centre Bretagne | Site internet et logiciel sur mesure",
+    description:
+        "Développeur freelance en Centre Bretagne. Création de site internet et développement logiciel sur mesure pour entreprises en Bretagne.",
+    path: "/",
+    keywords: seoKeywords,
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(siteUrl),
-  title: {
-    default:
-      "Développeur freelance en Centre Bretagne | Site internet et logiciel sur mesure",
-    template: "%s | Antoine Souesme",
-  },
-  description: defaultMetadata.description,
-  keywords: seoKeywords,
-  alternates: defaultMetadata.alternates,
-  openGraph: defaultMetadata.openGraph,
-  twitter: defaultMetadata.twitter,
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-snippet": -1,
-      "max-image-preview": "large",
-      "max-video-preview": -1,
+    metadataBase: new URL(siteUrl),
+    title: {
+        default:
+            "Développeur freelance en Centre Bretagne | Site internet et logiciel sur mesure",
+        template: "%s | Antoine Souesme",
     },
-  },
-  authors: [{ name: "Antoine Souesme" }],
-  creator: "Antoine Souesme",
-  publisher: "Antoine Souesme",
+    description: defaultMetadata.description,
+    keywords: seoKeywords,
+    alternates: defaultMetadata.alternates,
+    openGraph: defaultMetadata.openGraph,
+    twitter: defaultMetadata.twitter,
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-snippet": -1,
+            "max-image-preview": "large",
+            "max-video-preview": -1,
+        },
+    },
+    authors: [{ name: "Antoine Souesme" }],
+    creator: "Antoine Souesme",
+    publisher: "Antoine Souesme",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+            <body>{children}</body>
+
+            {/* https://vercel.com/docs/analytics/package */}
+            <Analytics />
+        </html>
+    );
 }
